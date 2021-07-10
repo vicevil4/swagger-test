ff9a18fc17ea8f8b4a5606e12d0d95871ed6dfb6
Simon Jung <vicevil4@gmail.com>
2021-07-10 17:07:01 +0900

    Initial import

---
+++ src/test/java/io/vicevil4/swagger/SwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vicevil4.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
